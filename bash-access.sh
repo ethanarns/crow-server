1718409ec23cdbc27aeae3426b2c1bb60e8adc44
Ethan Arns <narkro555@gmail.com>
2018-10-17 13:15:37 -0700

    Dependencies added

---
+++ bash-access.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -ti crowbox:latest bash
---
+++ server/main.cpp
@@ -0,0 +1,8 @@
+#include "crow_all.h"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    crow:SimpleApp app;
+    return 0;
+}
